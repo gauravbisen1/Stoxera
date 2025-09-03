8b21b24b8f5625dbf0df255917ee9e755914fc2d
gauravbisen1 <gouravbisen123@gmail.com>
2025-09-03 23:16:24 +0530

    .

--- frontend/src/landing_page/about/Header.jsx
+++ frontend/src/landing_page/about/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='container'>
+      <div className="row">
+        
+      </div>
+    </div>
   )
 }
 

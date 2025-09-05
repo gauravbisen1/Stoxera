eedd8bd18c939e8b707dc8a5d5b9b2f2f376de7f
gauravbisen1 <gouravbisen123@gmail.com>
2025-09-05 22:56:53 +0530

    backend

--- backend/App.js
+++ backend/App.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;//for specify when we deply port will change
+const URL = process.env.MONGO_URL;
 
 const app = express();
 
-app.listen(8080 , ()=>{
-    console.log("server is running on 8080")
+app.listen(PORT , ()=>{
+    console.log("server is running on 8080");
+    mongoose.connect(URL);
+    console.log("DB connected!");
 })
